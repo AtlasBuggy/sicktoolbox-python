5cb06c3e89bfe1f3e9da254a1cd5cea1a281ffc9
Woz4tetra <woz4tetra@gmail.com>
2017-08-15 22:31:18 -0400

    created gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Woz4tetra/Documents/Code/LMS200/sicktoolbox-python
-# Build directory: /Users/Woz4tetra/Documents/Code/LMS200/sicktoolbox-python/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
