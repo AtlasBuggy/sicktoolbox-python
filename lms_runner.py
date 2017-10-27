721199d6ac74761790505f867adad40636954daa
Woz4tetra <woz4tetra@gmail.com>
2017-10-27 17:42:57 -0400

    Changed port to work with the jetson

--- lms_runner.py
+++ lms_runner.py
@@ -18,7 +18,7 @@ map_size_meters = 50
 # file_name = "2017_Sep_16/LMS200/12;17;56.log"
 file_name = "2017_Sep_16/LMS200/13;28;47.log"
 
-slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=True)
+slam = Slam(map_size_pixels, map_size_meters, write_image=False, enabled=True)
 
 
 class PlaybackOrchestrator(Orchestrator):
@@ -35,8 +35,8 @@ class LiveOrchestrator(Orchestrator):
         super(LiveOrchestrator, self).__init__(event_loop)
 
         sicklms = LMS200(
-            # "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0"
-            "/dev/cu.usbserial"
+            "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0"
+            # "/dev/cu.usbserial"
         )
         self.add_nodes(sicklms, slam)
         self.subscribe(sicklms, slam, slam.lms_tag)
